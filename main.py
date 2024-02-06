6b728bf3f949d5374b6d1e854a1d8297ef7a5096
evtish <evevtish@gmail.com>
2024-02-06 20:51:55 +0300

    sigma update

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from pyrogram import Client, filters, enums
 #from create_session import session_name
 
+'''proxy = {
+     'scheme': 'http',
+     'hostname': 'evtish.pythonanywhere.com',
+     'port': 3128,
+     'username': 'username',
+     'password': 'password'
+}
+
+bot = Client('mentioning_all_members', proxy=proxy)'''
 bot = Client('mentioning_all_members')
 
 with open('info.txt', encoding='utf-8') as info_file:
@@ -16,8 +25,8 @@ async def get_all_members(client, message):
 
         async for member in bot.get_chat_members(chat_id):
             curr_username = member.user.username
-            if not member.user.is_bot and curr_username != sender_username:
-                members.append('@' + curr_username)
+            if curr_username and not member.user.is_bot and curr_username != sender_username:
+                members.append('@' + str(curr_username))
 
         stop_printing = False
         while not stop_printing:
@@ -30,6 +39,8 @@ async def get_all_members(client, message):
                 stop_printing = True
 
             await bot.send_message(chat_id, printing_members, reply_to_message_id=message.reply_to_message_id)
+        '''for name in members:
+            await bot.send_message(chat_id, name, reply_to_message_id=message.reply_to_message_id)'''
 
     else:
         await bot.send_message(message.chat.id, 'Эта команда предназначена только для групп')
@@ -43,6 +54,11 @@ async def info(client, message):
 @bot.on_message(filters.command('skibidi'))
 async def skibidi(client, message):
     await bot.send_message(message.chat.id, 'брр скибиди доп доп доп ес ес')
+
+
+@bot.on_message(filters.command('sigma'))
+async def sigma(client, message):
+    await bot.send_animation(message.chat.id, 'other files/sigma.gif')
 
 
 bot.run()
